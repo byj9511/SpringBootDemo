1b239fd95992e9705f5d61eecbdc94d43fd99e63
byj <18621711850@163.com>
2020-04-08 18:15:06 +0800

    删除target

--- src/main/java/com/refactor/demo/Dao/DepartmentMapper.java
+++
@@ -1,9 +0,0 @@
-package com.refactor.demo.Dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.refactor.demo.entities.Department;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentMapper extends BaseMapper<Department> {
-}
